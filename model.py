b0f63c038235695505646a0ffbe29a653d0caa0e
Minza-Ilahi <ilahiminza@gmail.com>
2026-02-25 23:23:37 +0530

    Fix immune system function to include N and correct calculations

--- model.py
+++ model.py
@@ -1,10 +1,11 @@
 # SRC
 import numpy as np
 def immune_system(y, t, alpha, beta, delta):
-  A, T = y
+  A, N, T = y
    
-  dA_dt = -delta A
+  dA_dt = -delta * A
+  dN_dt = -alpha * A * N
   dT_dt = alpha * A - beta * T
   
-  return[dA_dt, dT_dt]
+  return[dA_dt, dN_dt, dT_dt]
 
